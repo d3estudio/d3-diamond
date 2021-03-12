34c9ad8983e2fae1cc5facdb132ce34b7ce237b4
Plankiton <pl4nk1ton@gmail.com>
2021-03-12 11:43:42 -0300

    Add query functions and relationship between Score and User

--- api/model_score.go
+++ api/model_score.go
@@ -53,3 +53,54 @@ func (model *Score) Update(columns sex.Dict) bool {
 
     return false
 }
+
+func (model *UserScore) Create() bool {
+    if (model.ModelType == "") {
+        model.ModelType = sex.GetModelType(model)
+    }
+
+    if sex.ModelCreate(model) == nil {
+        ID := model.ID
+        ModelType := model.ModelType
+        sex.Log("Created", sex.ToLabel(ID, ModelType))
+        return true
+    }
+
+    return false
+}
+
+func (model *UserScore) Delete() bool {
+    ID := model.ID
+    ModelType := model.ModelType
+
+    if sex.ModelCreate(model) == nil {
+        sex.Log("Deleted", sex.ToLabel(ID, ModelType))
+        return true
+    }
+
+    return false
+}
+
+func (model *UserScore) Save() bool {
+    ID := model.ID
+    ModelType := model.ModelType
+
+    if sex.ModelSave(model) == nil {
+        sex.Log("Updated", sex.ToLabel(ID, ModelType))
+        return true
+    }
+
+    return false
+}
+
+func (model *UserScore) Update(columns sex.Dict) bool {
+    ID := model.ID
+    ModelType := model.ModelType
+
+    if sex.ModelUpdate(model, columns) == nil {
+        sex.Log("Updated", sex.ToLabel(ID, ModelType))
+        return true
+    }
+
+    return false
+}
--- api/score.go
+++ api/score.go
@@ -9,4 +9,142 @@ type Score struct {
     Name       string `json:"name"`
 }
 
+type UserScore struct {
+    sex.Model
+    UserId     uint
+    ScoreId    uint
+}
 
+func (self UserScore) Sign(user User, score Score) (User, Score) {
+    self.UserId = user.ID
+    self.ScoreId = score.ID
+
+    self.Create()
+    sex.Log("Linked", sex.ToLabel(user.ID, user.ModelType), user.Name, "to", sex.ToLabel(score.ID, score.ModelType), score.Name)
+
+    return user, score
+}
+
+func (self UserScore) Unsign(user User, score Score) (User, Score) {
+    self.UserId = user.ID
+    self.ScoreId = score.ID
+
+    self.Delete()
+    sex.Log("Unlinked", sex.ToLabel(user.ID, user.ModelType), user.Name, "from", sex.ToLabel(score.ID, score.ModelType), score.Name)
+
+    return user, score
+}
+
+func (self Score) Sign(user User) (User, Score) {
+    link := UserScore{}
+    user, self = link.Sign(user, self)
+
+    return user, self
+}
+
+func (self Score) Unsign(user User) (User, Score) {
+    link := UserScore{}
+    e := db.Where("user_id = ? AND score_id = ?", user.ID, self.ID).First(&link)
+    if e.Error == nil {
+        user, self = link.Unsign(user, self)
+    }
+
+    return user, self
+}
+
+func (self *Score) GetUsers(page int, limit int) []User {
+    e := db.First(self)
+    if e.Error == nil {
+        user_list := []uint{}
+        users := []User{}
+        e := db.Raw("SELECT u.id FROM users u INNER JOIN user_scores ur INNER JOIN scores r ON ur.score_id = r.id AND ur.user_id = u.id AND r.id = ?", self.ID)
+        if limit > 0 && page > 0 {
+            e = e.Offset((page-1)*limit).Limit(limit)
+        }
+        e = e.Find(&user_list)
+
+        if e.Error == nil {
+            e := db.Find(&users, "id in ?", user_list)
+            if e.Error == nil {
+                return users
+            }
+        }
+        if e.Error == nil {
+            e := db.Find(&users, "id in ?", user_list)
+            if e.Error == nil {
+                return users
+            }
+        }
+    }
+
+    return []User{}
+}
+
+func (self *User) GetScores(page int, limit int) []Score {
+    e := db.First(self)
+    if e.Error == nil {
+        score_list := []uint{}
+        scores := []Score{}
+        e := db.Raw("SELECT r.id FROM scores r INNER JOIN user_scores ur INNER JOIN users u ON ur.score_id = r.id AND ur.user_id = u.id AND u.id = ?", self.ID)
+        if limit > 0 && page > 0 {
+            e = e.Offset((page-1)*limit).Limit(limit)
+        }
+        e = e.Find(&score_list)
+
+        if e.Error == nil {
+            e := db.Find(&scores, "id in ?", score_list)
+            if e.Error == nil {
+                return scores
+            }
+        }
+    }
+
+    return []Score{}
+}
+
+
+func (self *Score) QueryUsers(page int, limit int, query ...interface{}) []User {
+    e := db.First(self)
+    if e.Error == nil {
+        user_list := []uint{}
+        users := []User{}
+        e := db.Raw("SELECT u.id FROM users u INNER JOIN user_scores ur INNER JOIN scores r ON ur.score_id = r.id AND ur.user_id = u.id AND r.id = ?", self.ID)
+        if limit > 0 && page > 0 {
+            e = e.Offset((page-1)*limit).Limit(limit)
+        }
+
+        e = e.Find(&user_list, query...)
+
+        if e.Error == nil {
+            e := db.Find(&users, "id in ?", user_list)
+            if e.Error == nil {
+                return users
+            }
+        }
+    }
+
+    return []User{}
+}
+
+
+func (self *User) QueryScores(page int, limit int, query...interface{}) []Score {
+    e := db.First(self)
+    if e.Error == nil {
+        score_list := []uint{}
+        scores := []Score{}
+        e := db.Raw("SELECT r.id FROM scores r INNER JOIN user_scores ur INNER JOIN users u ON ur.score_id = r.id AND ur.user_id = u.id AND u.id = ?", self.ID)
+        if limit > 0 && page > 0 {
+            e = e.Offset((page-1)*limit).Limit(limit)
+        }
+        e = e.Find(&score_list, query...)
+
+        if e.Error == nil {
+            e := db.Find(&scores, "id in ?", score_list)
+            if e.Error == nil {
+                return scores
+            }
+        }
+    }
+
+    return []Score{}
+}
