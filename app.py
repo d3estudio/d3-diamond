13832d631ee3906ff7765c3d4d110d067030f178
Plankiton <pl4nk1ton@gmail.com>
2021-03-24 19:06:21 -0300

    Fix csv convertions for diamond show

--- app.py
+++ app.py
@@ -10,8 +10,60 @@ from flask import \
 
 app = Flask(__name__, static_url_path='/static/')
 
+def load_scores(user, token):
+    try:
+        scores = get("http://api:8000/user/{}/scores".format(user["id"]), headers={
+            "Authorization": token
+        }).json()['data']
+    except:
+        scores = []
+    info = get("http://api:8000/score-type", headers={"Authorization":token}).json()['data']
+
+    indices = {}
+
+    i = 0
+    l = "abcdefghijklmnopqrstuvwxyz"
+    scores_formated = {}
+    for j in info:
+        indices[l[i]] = j
+        for s in scores:
+            if s['name'].lower() == j['name'].lower():
+                if not "value" in s:
+                    s["value"] = 0
+                if not s['name'].lower() in scores_formated:
+                    scores_formated[s['name'].lower()] = s
+                    scores_formated[s['name'].lower()]["total"] = 1
+                else:
+                    scores_formated[s['name'].lower()]["value"] += s["value"]
+                    scores_formated[s['name'].lower()]["total"] += 1
+
+        i += 1
+    return [scores_formated, info, indices]
+
 def log(*args, **kwargs):
     print(*args, **kwargs, file=open("app.log", "a"))
+
+def gen_csv(indices, scores):
+    # 'ID,NAME,DATE,A,B,C,D,E,F,G,H,I,J,CHANGE_A,CHANGE_B,CHANGE_C,CHANGE_D,CHANGE_E,CHANGE_F,CHANGE_G,CHANGE_H,CHANGE_I,CHANGE_J\n'
+    csv = ',,,'
+
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    for l in letters:
+        if l in indices:
+            i = indices[l]
+            csv += "\"%.1f\"," % scores[i["name"]]["value"]
+    for l in letters:
+        if l in indices:
+            i = indices[l]
+            if 'last' in scores[i["name"]]:
+                csv += str(scores[i["name"]]["last"]) + ','
+            else:
+                csv += "0,"
+    if csv[-1] == ',':
+        csv = csv[:-1]
+
+    log(csv)
+    return csv
 
 @app.route('/dates', methods=['GET'])
 def date_filters():
@@ -74,42 +126,18 @@ def login():
 
     return render("login.html", error=["Falha no login", response.json()['message']])
 
+
 @app.route('/', methods=['GET'])
 def index():
     token = r.cookies.get("d3diamond_token")
     response = post("http://api:8000/verify", headers={"Authorization":token})
     if response.status_code == 200:
         user = response.json()['data']
-        try:
-            scores = get("http://api:8000/user/{}/scores".format(user["id"]), headers={
-                "Authorization": token
-            }).json()['data']
-        except:
-            scores = []
-        info = get("http://api:8000/score-type", headers={"Authorization":token}).json()['data']
+        scores, info, indices = load_scores(user, token)
 
-        indices = {}
-
-        i = 0
-        l = "abcdefghijklmnopqrstuvwxyz"
-        scores_formated = {}
-        for j in info:
-            indices[l[i]] = j
-            for s in scores:
-                if s['name'].lower() == j['name'].lower():
-                    if not "value" in s:
-                        s["value"] = 0
-                    if not s['name'].lower() in scores_formated:
-                        scores_formated[s['name'].lower()] = s
-                        scores_formated[s['name'].lower()]["total"] = 1
-                    else:
-                        scores_formated[s['name'].lower()]["value"] += s["value"]
-                        scores_formated[s['name'].lower()]["total"] += 1
-
-            i += 1
-
-        return render("index.html", user = user, indices = indices, scores = scores_formated)
+        return render("index.html", user = user, indices = indices, scores = scores, csv = gen_csv(indices, scores))
     return redirect("/login")
+
 
 @app.route("/<date>", methods = ["GET"])
 def filter_by_date(date):
@@ -120,38 +148,7 @@ def filter_by_date(date):
     response = post("http://api:8000/verify", headers={"Authorization":token})
     if response.status_code == 200:
         user = response.json()['data']
+        scores, info, indices = load_scores(user, token)
 
-        try:
-            scores = get("http://api:8000/user/{}/scores".format(user["id"]), headers={
-                "Authorization": token,
-                "Query": "created_at BETWEEN '{}' AND '{}'".format(
-                    date.isoformat(), dateend.isoformat()
-                )
-            }).json()['data']
-        except:
-            scores = []
-
-        info = get("http://api:8000/score-type", headers={"Authorization":token}).json()['data']
-
-        indices = {}
-
-        i = 0
-        l = "abcdefghijklmnopqrstuvwxyz"
-        scores_formated = {}
-        for j in info:
-            indices[l[i]] = j
-            for s in scores:
-                if s['name'].lower() == j['name'].lower():
-                    if not "value" in s:
-                        s["value"] = 0
-                    if not s['name'].lower() in scores_formated:
-                        scores_formated[s['name'].lower()] = s
-                        scores_formated[s['name'].lower()]["total"] = 1
-                    else:
-                        scores_formated[s['name'].lower()]["value"] += s["value"]
-                        scores_formated[s['name'].lower()]["total"] += 1
-
-            i += 1
-
-        return render("index.html", user = user, indices = indices, scores = scores_formated)
+        return render("index.html", user = user, indices = indices, scores = scores, csv = gen_csv(indices, scores))
     return redirect("/login")
